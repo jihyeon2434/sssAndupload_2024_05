458d98340ff4e1cbb0d31a73ee61428df8e1242e
jihyeon2434 <ubis970420@gmail.com>
2024-05-20 14:16:37 +0900

    프로젝트 세팅

---
+++ src/main/java/com/koreait/sssandupload/SssAnduploadApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.sssandupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SssAnduploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SssAnduploadApplication.class, args);
+    }
+
+}
