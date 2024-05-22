b3a91b826b9a8d83fa762e8d8011d9da9977b86b
jihyeon2434 <ubis970420@gmail.com>
2024-05-22 17:41:25 +0900

    GET 요청의 응답 본문엔 안녕이 나와야 함

--- src/main/java/com/koreait/sssandupload/app/member/controller/MemberController.java
+++ src/main/java/com/koreait/sssandupload/app/member/controller/MemberController.java
@@ -3,15 +3,24 @@ package com.koreait.sssandupload.app.member.controller;
 import com.koreait.sssandupload.app.member.entity.Member;
 import com.koreait.sssandupload.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/join")
     public String showJoin() {
@@ -19,16 +28,35 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    @ResponseBody
-    public String join(String username, String password, String email, MultipartFile profileImg) {
+    public String join(HttpServletRequest req, String username, String password, String email, MultipartFile profileImg) {
         Member oldMember = memberService.getMemberByUsername(username);
 
+        String passwordClearText = password;
+        password = passwordEncoder.encode(password);
+
         if (oldMember != null) {
-            return "이미 있음";
+            return "redirect:/?errorMsg=AlreadyExists";
         }
 
-        Member member = memberService.join(username, "{noop}" + password, email, profileImg);
+        Member member = memberService.join(username, password, email, profileImg);
 
-        return "success";
+        try {
+            req.login(username, passwordClearText);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        }
+
+        return "redirect:/member/profile";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/profile")
+    public String showProfile(Principal principal, Model model) {
+
+        Member loginedMember = memberService.getMemberByUsername(principal.getName());
+
+        model.addAttribute("loginedMember", loginedMember);
+
+        return "member/profile";
     }
 }
